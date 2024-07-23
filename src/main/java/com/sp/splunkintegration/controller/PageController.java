2c7eff2c55d4838fed260053b924c812a54b5cc4
lslaoang <laurellaoang@gmail.com>
2024-07-23 19:07:34 +0800

    use logback

--- src/main/java/com/sp/splunkintegration/controller/PageController.java
+++ src/main/java/com/sp/splunkintegration/controller/PageController.java
@@ -1,7 +1,7 @@
 package com.sp.splunkintegration.controller;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PageController {
 
 
-    Logger LOGGER = LogManager.getLogger(PageController.class);
+    Logger LOGGER = LoggerFactory.getLogger(PageController.class);
 
     @GetMapping("/")
     public String getIndex(){
