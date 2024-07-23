2041aa1d367ffbfb807442c70cf8188ae1b09c33
lslaoang <laurellaoang@gmail.com>
2024-07-23 18:13:20 +0800

    configure logging via log4j2

--- src/main/java/com/sp/splunkintegration/controller/PageController.java
+++ src/main/java/com/sp/splunkintegration/controller/PageController.java
@@ -1,13 +1,27 @@
 package com.sp.splunkintegration.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PageController {
 
+
+    Logger LOGGER = LogManager.getLogger(PageController.class);
+
     @GetMapping("/")
     public String getIndex(){
+        LOGGER.info("getIndex, You got here.");
         return "Hello World";
+    }
+
+    @GetMapping("/getLogs")
+    public String getLogs(){
+        LOGGER.info("getLogs: {}", System.currentTimeMillis());
+        LOGGER.info("getLogs: {}", "Some awesome logs.");
+        LOGGER.info("getLogs: {}", "Some meaningful logs.");
+        return "You got logs!";
     }
 }
